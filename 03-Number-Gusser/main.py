598c2976e16538f260796a543ddff467cfde7c0b
majorzero335 <dogmanpete1542@gmail.com>
2025-03-07 23:41:17 +0000

    Initial Commit

--- 03-Number-Gusser/main.py
+++ 03-Number-Gusser/main.py
@@ -5,9 +5,13 @@ from random import randrange
 randrange(10)
 print("I am guessing a number from 1-10. Can you guess the number?")
 while True:
-    guess = int(input())
-    if guess == randrange(10):
+    guess = int(input("Enter your guess:"))
+    if guess < randrange(10):
+        print("Your guess is too low")
+    elif guess > randrange(10):
+        print("Your guess is too high")
+        
+    else:
         print("You guessed it!")
         break
-    else:
-        print("Try again")
+        
