446fd14e261016bf8dc7f4b17f2d2a988b31ded2
majorzero335 <dogmanpete1542@gmail.com>
2025-03-13 01:43:41 +0000

    Initial Commit

--- 03-Number-Gusser/main.py
+++ 03-Number-Gusser/main.py
@@ -1,3 +1,4 @@
+maxtries = 5
 attempts = 0
 print("what is your name?")
 if input():
@@ -5,18 +6,17 @@ if input():
 import random
 str = random.randint(1, 10)
 print("I am guessing a number from 1-10. Can you guess the number?")
-print("You can only get 10 tries")
-while attempts < 5:
+print(f"You can only get {maxtries} tries")
+while attempts < maxtries:
     guess = int(input("Please guess a number"));
-    attempts_used= attempts + 1;
+    attempts= attempts + 1;
     if guess > str:
             print("Guess is too high, try a smaller number");
     elif guess < str:
             print("Guess is too low, try a higher number");
     elif guess == str:
-            attempts_used=str(attempts_used)
-            print("Correct- you win in", attempts_used, "guesses");
+            print(f"Correct- you win in {attempts} guesses");
             exit();
 else:
-    if attempts == 5:
+    if attempts == maxtries:
        print("You failed to guess in time") 
