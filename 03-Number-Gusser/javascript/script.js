bef04e32f61ca4e8425aa35507394822c5d90b2b
majorzero335 <dogmanpete1542@gmail.com>
2025-04-01 11:28:54 -0500

    Made changes to JavaScript and Python files to be similar

--- 03-Number-Gusser/javascript/script.js
+++ 03-Number-Gusser/javascript/script.js
@@ -1,23 +1,23 @@
 var attempts = 0;
 const maxtries = 5;
-var name = prompt("What is your name?");
-alert("Hello, " + name + "!");
+var n = prompt("What is your name?");
+alert("Hello, " + n + "!");
 alert("Welcome to the number guessing game!");
-var str = Math.floor(Math.random() * 10) + 1;
+var number = Math.floor(Math.random() * 10) + 1;
 alert("I am guessing a number from 1-10. Can you guess the number?");
 alert("You can only get " + maxtries + " tries");
 while (attempts < maxtries) {
     var guess = prompt("Please Guess a number");
     guess = parseInt(guess);
     attempts += 1;
-    if (guess > str) {
+    if (guess > number) {
         alert("Guess is too high, try a smaller number");
-    } else if (guess < str) {
+    } else if (guess < number) {
         alert("Guess is too low, try a higher number");
-    } else if (guess == str) {
+    } else if (guess == number) {
         alert("Correct- you win in " + attempts + " guesses");
         break;
     } else {
-        alert("You failed to guess in time. The correct number was " + str + ". Better luck next time!"); // This line will execute if the user fails to guess correctly within the allowed attempts
+        alert("You failed to guess in time. The correct number was " + number + ". Better luck next time!");
     }
 }
--- 03-Number-Gusser/python/main.py
+++ 03-Number-Gusser/python/main.py
@@ -4,19 +4,19 @@ print("What is your name?")
 print("Hello, " + input() + "!")
 print("Welcome to the number guessing game!")
 import random
-str = random.randint(1, 10)
+number = random.randint(1, 10)
 print("I am guessing a number from 1-10. Can you guess the number?")
 print(f"You can only get {maxtries} tries")
 while attempts < maxtries:
     guess = int(input("Please guess a number:"));
     attempts= attempts + 1;
-    if guess > str:
+    if guess > number:
             print("Guess is too high, try a smaller number");
-    elif guess < str:
+    elif guess < number:
             print("Guess is too low, try a higher number");
-    elif guess == str:
+    elif guess == number:
             print(f"Correct- you win in {attempts} guesses");
             exit();
 else:
     if attempts == maxtries:
-       print("You failed to guess in time") 
+       print("You failed to guess in time. The correct number was " + number + ". Better luck next time!") 
