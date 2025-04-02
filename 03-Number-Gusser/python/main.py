f2d90caf0ed358246fe85701fbe9ee66fd97ab9b
Aadi Patel <dogmanpete1542@gmail.com>
2025-04-02 16:37:34 -0500

    Refactor variable names for clarity and improve user feedback in number guessing game

--- 03-Number-Gusser/python/main.py
+++ 03-Number-Gusser/python/main.py
@@ -1,22 +1,22 @@
 maxtries = 5
 attempts = 0
-print("What is your name?")
-print("Hello, " + input() + "!")
+n = input("What is your name?")
+print("Hello, " + n + "!")
 print("Welcome to the number guessing game!")
 import random
-str = random.randint(1, 10)
+number = random.randint(1, 10)
 print("I am guessing a number from 1-10. Can you guess the number?")
 print(f"You can only get {maxtries} tries")
 while attempts < maxtries:
-    guess = int(input("Please guess a number:"));
-    attempts= attempts + 1;
-    if guess > str:
-            print("Guess is too high, try a smaller number");
-    elif guess < str:
-            print("Guess is too low, try a higher number");
-    elif guess == str:
-            print(f"Correct- you win in {attempts} guesses");
+    guess = int(input("Please guess a number:"))
+    attempts= attempts + 1
+    if guess > number:
+            print("The guess is too high, try a smaller number")
+    elif guess < number:
+            print("The guess is too low, try a higher number")
+    elif guess == number:
+            print("Congratulations- you win in " + attempts + "guesses")
             exit();
 else:
     if attempts == maxtries:
-       print("You failed to guess in time") 
+       print("You failed to guess in time. The correct number was " + number + ". Better luck next time!") 
