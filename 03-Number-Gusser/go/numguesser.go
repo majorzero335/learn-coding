8e92163458b31dd1f70f80ece83d930bbe4e15e1
majorzero335 <dogmanpete1542@gmail.com>
2025-04-02 17:11:27 -0500

    Update numguesser.go (first step of drafting)

---
+++ 03-Number-Gusser/go/numguesser.go
@@ -0,0 +1,7 @@
+const maxtries = 5
+var attempts = 0
+var name = prompt("What is your name?")
+print("Hello " + name + "!")
+print("Welcome to the number guessing game!")
+print("I am thinking of a number between 1 and 10. Can you guess it?")
+print("You have " + maxtries + " tries to guess the number.")
