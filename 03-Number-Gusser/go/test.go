11d704b118b288b5502073404c43dce2a0bb838a
majorzero335 <dogmanpete1542@gmail.com>
2025-04-22 18:08:32 -0500

    creating a test.go program

--- 03-Number-Gusser/go/test.go
+++ 03-Number-Gusser/go/test.go
@@ -1,12 +1,7 @@
-cd %HOMEPATH%
-mkdir hello
-cd hello
-$ go mod init example/hello
-go: creating new go.mod: module example/hello
 package main
 
 import "fmt"
 
 func main() {
-    fmt.Println("Hello, World!")
+    fmt.Println("Hello World!")
 }
